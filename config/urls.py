11838d2fbbf3ffe0367fdda0aefaa552aec1c780
Nurbol077 <kadyrbekovnurbol002@gmail.com>
2025-03-18 17:23:37 +0600

    django_projekt

--- config/urls.py
+++ config/urls.py
@@ -15,12 +15,13 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 from django.conf.urls.static import static
 from django.conf import settings
 from django.shortcuts import render
 
 from account.views import register
+
 
 def home(request):
     from account.models import User
@@ -32,6 +33,7 @@ urlpatterns = [
     path('', home, name='home'),
     path('admin/', admin.site.urls),
     path('register/', register, name='register'),
+    path('products/', include('product.urls'))
 
 ]
 
---
+++ product/urls.py
@@ -0,0 +1,6 @@
+from django.urls import path
+from product.views import product_list
+
+urlpatterns =[
+    path('', product_list, name = 'product_list')
+]
