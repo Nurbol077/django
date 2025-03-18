0f58d47e90433ed99b2900c1645d86ca3c7b81f9
Nurbol077 <kadyrbekovnurbol002@gmail.com>
2025-03-18 17:43:55 +0600

    +

--- product/views.py
+++ product/views.py
@@ -1,4 +1,7 @@
 from django.shortcuts import render
+import requests
 
 def product_list(request):
-    return render(request, 'product/product_list.html')
+    products = requests.get('https://boykot.kg/api/v1/products/').json()
+    return render(request, 'product/product_list.html', {'products': products})
+
